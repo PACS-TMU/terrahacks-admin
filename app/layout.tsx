14735f0bcfc5abfedc0920aec53bdc8c8d4e5ecb
monakouzi <mo.nakouzi03@gmail.com>
2024-07-11 22:00:09 -0400

    fixing server side error

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const defaultUrl = process.env.VERCEL_URL
   : "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
+  metadataBase: new URL('https://admin.terrahacks.ca'),
   title: "Next.js and Supabase Starter Kit",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
