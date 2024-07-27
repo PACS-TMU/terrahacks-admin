85e9ec9dd0e0f82bfa37cc6ab735fc146375d149
monakouzi <mo.nakouzi03@gmail.com>
2024-07-26 22:24:59 -0400

    Making loading better
    Co-authored-by: William Khine <williamkhine@outlook.com>

--- components/check-in/checkin-button.tsx
+++ components/check-in/checkin-button.tsx
@@ -3,6 +3,8 @@ import { createClient } from "@/utils/supabase/client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
 const supabase = createClient();
 
 export default function CheckinButton({
@@ -23,25 +25,24 @@ export default function CheckinButton({
             .select();
 
         if (error) {
-            setPending(false);
             return router.replace(
                 "/dashboard?error=Check-in failed: " + error.message
             );
         } else {
-            setPending(false);
             return router.refresh();
         }
     };
 
     return (
         <button
-            className={`border w-11/12 lg:w-1/2 px-4 py-2 rounded-md shadow-sm duration-300 ease-in-out text-2xl ${checkedIn
+            className={`border w-11/12 lg:w-1/2 px-4 py-2 rounded-md shadow-sm duration-300 ease-in-out text-2xl flex flex-row justify-center items-center ${checkedIn
                     ? "bg-red-500 text-white hover:bg-red-500 pointer-events-none line-through"
                     : "hover:bg-gray-300"
                 }`}
             disabled={checkedIn !== null}
             onClick={checkIn}
         >
+            {pending && <AiOutlineLoading3Quarters className="animate-spin mr-2" />}
             {pending ? "Checking in..." : checkedIn !== null ? "Checked in" : "Check in"}
         </button>
     );
--- components/meals/meal-button.tsx
+++ components/meals/meal-button.tsx
@@ -3,6 +3,8 @@ import { createClient } from "@/utils/supabase/client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
 const supabase = createClient();
 
 export default function MealButton({ mealNumber, meal_id, mealTaken, admin_id }: {
@@ -29,10 +31,8 @@ export default function MealButton({ mealNumber, meal_id, mealTaken, admin_id }:
         }).eq('meal_id', meal_id).select();
 
         if (error) {
-            setPending(false);
             return router.replace('/dashboard?error=Error taking meal: ' + error.message);
         } else {
-            setPending(false);
             return router.refresh()
         }
 
@@ -42,8 +42,9 @@ export default function MealButton({ mealNumber, meal_id, mealTaken, admin_id }:
         <button
             disabled={mealTaken}
             onClick={takeMeal}
-            className={`border w-full px-4 py-2 rounded-md shadow-sm duration-300 ease-in-out ${mealTaken ? 'bg-red-500 text-white line-through' : 'hover:bg-gray-300'}`}
+            className={`border w-full px-4 py-2 rounded-md shadow-sm duration-300 ease-in-out flex flex-row justify-center items-center ${mealTaken ? 'bg-red-500 text-white line-through' : 'hover:bg-gray-300'}`}
         >
+            {pending && <AiOutlineLoading3Quarters className="animate-spin mr-2" />}
             {pending ? 'Taking Meal...' : mealTaken ? `Meal ${mealNumber} Taken` : `Take Meal ${mealNumber}`}
         </button>
     )
